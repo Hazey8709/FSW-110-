fe390262c532d3aad8a3aa0eafb09d2e61510cec
Hazey87 <88350421+Hazey8709@users.noreply.github.com>
2022-01-01 13:57:58 -0500

    Add files via upload

---
+++ Week-1/Select-Element/jav-El.js
@@ -0,0 +1,14 @@
+// Javascript:  Selecting-Elements
+
+var elgreet = document.getElementById("elgreet");
+elgreet.innerHTML =
+    "Hello, This is an example of Selecting Element's in JavaScript. ";
+
+/*
+function elgreet()
+{
+    document.getElementById("elgreet");
+    elgreet.innerHTML =
+        "Hello, This is an example of Selecting Element's in JavaScript. ";
+}
+*/
